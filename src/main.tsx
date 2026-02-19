349bda4936fa2616e113349ba2a10d148c941df9
Nayan Islam <iamnayanislam@gmail.com>
2026-02-19 15:01:06 +0600

    provide themes context

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,13 @@ import "./index.css";
 import App from "./App.tsx";
 import { CountersContextProvider } from "./Contexts/CountersContext.tsx";
 import { Toaster } from "react-hot-toast";
+import { ThemesContextProvider } from "./Contexts/ThemseContext.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <CountersContextProvider>
-    <Toaster />
+    <ThemesContextProvider>
+      <Toaster />
     <App />
+    </ThemesContextProvider>
   </CountersContextProvider>,
 );
